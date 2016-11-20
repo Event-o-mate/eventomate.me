1df9704574188d64deccecba0c25320d91255e9a
Simun Strukan <simun.strukan@gmail.com>
2016-11-20 02:09:53 +0100

    Fixed model typo

--- models/attendee.rb
+++ models/attendee.rb
@@ -2,7 +2,7 @@ class Attendee
 	include DataMapper::Resource
 
 	property :id, 				Serial
-	propert  :user_id			Integer	
+	propert  :user_id,		Integer	
 	property :created_on,	DateTime
 
 	has n, :events
