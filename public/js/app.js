37c32a522699ab4f27e1b31c362afb3bff843c77
Simun Strukan <simun.strukan@gmail.com>
2016-11-20 04:28:00 +0100

    making app

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,20 @@
 (function () {
   'use strict';
 
-  var loadAngularModules = [
-    "ngRoute"
-  ]
- 
-  angular
-    .module('eventomate', loadAngularModules)
-    .config(['$routeProvider', function($routeProvider){   
-      // Configure url routes
-      $routeProvider.
-      when('/', {
-      	//Admin dashboard
-        templateUrl:  'landing.html',
-        controller:   'LoginController',
-        controllerAs: 'login',
-      }).
-      when('/new_event', {
-        //Admin dashboard
-        templateUrl:  'install/install.html',
-        controller:   'InstallController',
-        controllerAs: 'login',
-      }).
-      when('/event_page', {
-        //Admin dashboard
-        templateUrl:  'event_page.html',
-        controller:   'EventController',
-        controllerAs: 'login',
-      }).
-      when('/dashboard', {
-        //Admin dashboard
-        templateUrl: 'dashboard.html',
-        controller: 'DashboardController',
-        controllerAs: "dashboard"
-      }).
-      otherwise({
-        redirectTo: '/'
+  var app = angular.module("eventomate", ["ngRoute"]);
+  app.config(function($routeProvider) {
+      $routeProvider
+      .when("/", {
+          templateUrl : "landing.html"
+      })
+      .when("/dashboard", {
+          templateUrl : "dashboard.html"
+      })
+      .when("/event_page", {
+          templateUrl : "event_page.html"
+      })
+      .when("/new_event", {
+          templateUrl : "new_event.html"
       });
-    }])
+  });
 })()
