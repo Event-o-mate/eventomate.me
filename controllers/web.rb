bda68d2c4a0ec827e69d4c150f310e93ddcbc36a
Simun Strukan <simun.strukan@gmail.com>
2016-11-20 02:48:24 +0100

    finding views

--- controllers/web.rb
+++ controllers/web.rb
@@ -7,7 +7,7 @@ require 'sinatra/assetpack'
 class WebController < Sinatra::Base
 	set :root, File.dirname(File.dirname(__FILE__))
 	set :public_folder, 'public'
-	# set :views, '/var/www/app/views'
+	set :views, 'views'
 	
 	# enable :static
 
