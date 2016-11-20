3f6a6d9a0ec0d3db32e993206b90c23df1de31c2
Simun Strukan <simun.strukan@gmail.com>
2016-11-20 06:21:59 +0100

    making app

--- public/js/main.ctrl.js
+++ public/js/main.ctrl.js
@@ -1,10 +1,3 @@
-(function () {
-	'use strict';
-
-  eventomate.controller('MainController', ['$scope', function($scope){
-  		$scope.test = "Simun ima brkove"
-
-  		console.log("Main Contrller " + test)
-  		console.log("Main  " + $scope.test)
-  }])
-})
+eventomate.controller('MainController', ['$scope', function($scope){
+		$scope.test = "Simun ima brkove"
+}])
